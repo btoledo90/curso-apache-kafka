82f458e64734cf44bbd7f1e7346c424a8c6fd203
btoledo90 <brunotq19@hotmail.com>
2024-06-07 22:42:06 -0300

    Add file

---
+++ str-producer - Copia/src/main/java/com/bruno/str_producer/services/StringProducerService.java
@@ -0,0 +1,19 @@
+package com.bruno.str_producer.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@RequiredArgsConstructor
+@Service
+public class StringProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        log.info("Send message {}", message);
+        kafkaTemplate.send("str-topic", message);
+    }
+}
---
+++ str-producer - Copia/src/test/java/com/bruno/str_producer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.str_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
